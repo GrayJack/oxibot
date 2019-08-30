45b647741cf505ee1bf675b909cff166dba55146
GrayJack <gr41.j4ck@gmail.com>
2019-08-30 19:34:29 -0300

    Improve fortune command

--- src/main.rs
+++ src/main.rs
@@ -271,15 +271,32 @@ fn latency(ctx: &mut Context, msg: &Message) -> CommandResult {
 #[command]
 #[description = "Tell a fortune"]
 #[bucket = "Meme"]
-fn fortune(ctx: &mut Context, msg: &Message) -> CommandResult {
-    let fortune = Command::new("fortune").arg("-s").output();
+fn fortune(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
+    let fortune = if args.is_empty() {
+        Command::new("fortune").arg("-s").output()
+    } else {
+        let arg = match args.single::<String>() {
+            Ok(a) => a,
+            Err(why) => {println!("Failed to get arg: {:?}", why); "".to_string()}
+        };
+        Command::new("fortune").args(vec!["-s", "-c", &arg]).output()
+    };
     let mut str = String::from("```\n");
     match fortune {
         Ok(out) => str.push_str(&out.stdout.iter().map(|&c| c as char).collect::<String>()),
-        Err(why) => println!("Error calling uname: {:?}", why),
+        Err(why) => {
+            println!("Error calling uname: {:?}", why);
+            str.push_str("Failed to get a fortune")
+        }
     };
 
-    str.push_str("\n```");
+    if str == "```\n" {
+        str.clear();
+        str.push_str("> No fortunes found :slight_frown: ");
+    } else {
+        str.push_str("\n```");
+    }
+
 
     msg.channel_id.say(&ctx.http, str)?;
 
